407bbb97c1358e71df467e4fc0a87d5e50994f8d
jdvpgii <gii.jdvp@gmail.com>
2023-09-23 06:14:50 +0500

    fix 500

--- src/main/java/ru/gapisovii/springcourse/HelloController.java
+++ src/main/java/ru/gapisovii/springcourse/HelloController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HelloController {
 
-    @GetMapping("/hello-world")
-    public String sayHello(){
-        return "hello_world";
+    @GetMapping("/hello")
+    public String sayHello() {
+        return "hello";
     }
 }
